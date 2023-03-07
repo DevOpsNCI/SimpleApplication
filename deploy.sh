bc06b77dc634ac554d476f7da265d3d82bcad6db
Gabriel S <gbss91@gmail.com>
2023-03-07 15:38:14 +0000

    Fixed issue with deploy pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.is with a built-in load balancer.
-sudo npm install -g pm2
+sudo npm install pm2 -g
 # stop any instance of our application running currently
 pm2 stop simple_app
 # change directory into folder where application is downloaded
